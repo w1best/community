d66b98a6e7b0e19c038e799b69658bf352ab7878
onelei <429280733s@gmail.com>
2020-02-02 17:24:52 +0800

    init repo

---
+++ src/main/java/wl/onelei/test/tolk/TolkApplication.java
@@ -0,0 +1,13 @@
+package wl.onelei.test.tolk;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TolkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wl/onelei/test/tolk/TolkApplicationTests.java
@@ -0,0 +1,13 @@
+package wl.onelei.test.tolk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
