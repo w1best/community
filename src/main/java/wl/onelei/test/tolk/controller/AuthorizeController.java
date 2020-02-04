77cd50611330ba38781794642d0900310c65be1f
onelei <429280733s@gmail.com>
2020-02-04 22:43:06 +0800

    持久化登录

--- src/main/java/wl/onelei/test/tolk/controller/AuthorizeController.java
+++ src/main/java/wl/onelei/test/tolk/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import wl.onelei.test.tolk.mapper.UserMapper;
 import wl.onelei.test.tolk.model.User;
 import wl.onelei.test.tolk.provider.GitHubProvider;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -35,25 +37,28 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO tokenDTO = new AccessTokenDTO();
         tokenDTO.setClient_id(clientId);
         tokenDTO.setClient_secret(secret);
         tokenDTO.setCode(code);
         tokenDTO.setRedirect_uri(redirectUri);
         tokenDTO.setState(state);
-        String token = gitHubProvider.getAccessToken(tokenDTO);
-        GitHubUser gitHubUser = gitHubProvider.getUser(token);
+        String gitHubToken = gitHubProvider.getAccessToken(tokenDTO);
+        GitHubUser gitHubUser = gitHubProvider.getUser(gitHubToken);
         System.out.println(gitHubUser.getName());
         if(gitHubUser != null){
             User user = new User();
             user.setName(gitHubUser.getName());
             user.setAccountId(String.valueOf(gitHubUser.getId()));
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insertUser(user);
-            request.getSession().setAttribute("user",gitHubUser);
+            response.addCookie(new Cookie("token",token));
+//            request.getSession().setAttribute("user",gitHubUser);
             return "redirect:/";
         }else {
             return "redirect:/";
--- src/main/java/wl/onelei/test/tolk/mapper/UserMapper.java
+++ src/main/java/wl/onelei/test/tolk/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package wl.onelei.test.tolk.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import wl.onelei.test.tolk.model.User;
 
 /**
@@ -17,4 +19,7 @@ import wl.onelei.test.tolk.model.User;
 public interface UserMapper {
     @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
     void insertUser(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
